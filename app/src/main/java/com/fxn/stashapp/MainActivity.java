9411a3572e6ca442a5f5d562bc182daad2889833
Akshay Sharma <fxn769@gmail.com>
2020-08-11 11:11:14 +0530

    androidX components Added

--- app/src/main/java/com/fxn/stashapp/MainActivity.java
+++ app/src/main/java/com/fxn/stashapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.fxn.stashapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fxn.adapters.UserAdapter;
 import com.fxn.modal.User;
@@ -13,38 +13,29 @@ import com.fxn.stash.Stash;
 
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     public static final String TAG_ARRAYLIST_STORE = "TAG_ARRAYLIST_STORE";
     RecyclerView recycleView;
     UserAdapter userAdapter;
-    @BindView(R.id.et1)
     EditText et1;
-    @BindView(R.id.et2)
     EditText et2;
 
-    @OnClick(R.id.store)
     public void store(View view) {
         Stash.put(TAG_ARRAYLIST_STORE, userAdapter.getList());
     }
 
-    @OnClick(R.id.get)
     public void get(View view) {
         ArrayList<User> mylist = Stash.getArrayList(TAG_ARRAYLIST_STORE, User.class);
         userAdapter.updateAll(mylist);
     }
 
-    @OnClick(R.id.clear)
     public void clear(View view) {
         Stash.clearAll();
         userAdapter.clearAll();
     }
 
-    @OnClick(R.id.addToList)
     public void addToList(View view) {
         if (et1.getText().toString().isEmpty()) {
             et1.setError("Name is Empty");
@@ -64,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         recycleView = findViewById(R.id.recycleView);
-        recycleView.setLayoutManager(new LinearLayoutManager(this));
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
         userAdapter = new UserAdapter();
         recycleView.setAdapter(userAdapter);
     }
